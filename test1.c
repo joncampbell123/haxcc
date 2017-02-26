6446fa0da87ed7decae60bde7cb17611f16f9a1c
Jonathan Campbell <jonathan@castus.tv>
2017-02-25 19:38:25 -0800

    multiply operator

--- test1.c
+++ test1.c
@@ -1,5 +1,5 @@
 
-int xyz,abc,faa,baa,wwwwwww;
+int xyz,abc,faa,baa = 10 * 0x111,wwwwwww;
 static long longcat;
 float floatey;
 double doubleback;
@@ -10,7 +10,7 @@ int efgh = 0x1234 - 0x1111;
 int morecomplex = 10000 + 2000 + 300 + 40 + 5 - 11111;
 
 void function1(void) {
-    int a;
+    int a = 2 * 4 * 8 * 16;
     int x = 246,xxx = 0x777;
     int y = x,yy = x,zzz = 0x1234,yyy = x;
     int z = notdefined;
