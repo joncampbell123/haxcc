e0c05c02b1d9ae628f9f7cd247a768ff889987bd
Jonathan Campbell <jonathan@castus.tv>
2017-03-06 16:07:39 -0800

    amusing test case for lineno capture.

--- test1.c
+++ test1.c
@@ -5,4 +5,8 @@ unsigned int abcu = 244U;
 const unsigned int caca = 1234U;
 static signed short int blah = 1222;
 long def = 12345678UL;
+static const
+    unsigned long
+        i_like_to_span_lines =
+            12345678UL;
 
